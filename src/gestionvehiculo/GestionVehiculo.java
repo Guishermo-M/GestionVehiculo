15ba4f156723375462b86f126aa8bd25f38dc058
Guishermo-M <wtf_156@hotmail.com>
2023-10-02 23:51:12 -0300

    Initial commit

---
+++ src/gestionvehiculo/GestionVehiculo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionvehiculo;
+
+/**
+ *
+ * @author WTF_1
+ */
+public class GestionVehiculo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("1.- Agregar Vehiculo");
+        System.out.println("2.- Actualizar modelo del vehículo");
+        System.out.println("3.- Imprimir todos los vehículos");
+        System.out.println("4.- Imprimir por categoría");
+        System.out.println("5.- Salir");
+    }
+    
+}
---
+++ src/gestionvehiculo/Vehiculo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionvehiculo;
+
+/**
+ *
+ * @author WTF_1
+ */
+public class Vehiculo extends Categoria{
+    private String patente;
+    private String modelo;
+    private Categoria categoria;
+    
+}
